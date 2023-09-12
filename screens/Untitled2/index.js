c5652b4d7e3ec919a86babfbfed914bf9319bd9f
Ravinder QA Lead <ravinder.y@crowdbotics.com>
2023-09-12 12:25:32 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,3 +1,5 @@
+import { TextInput } from "react-native";
+import { ActivityIndicator } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
@@ -8,13 +10,23 @@ const Untitled2 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><ActivityIndicator style={styles.UndKmSAt}></ActivityIndicator><TextInput style={styles.nJRUrEEU}></TextInput></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  UndKmSAt: {
+    width: 50,
+    height: 50
+  },
+  nJRUrEEU: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
   }
 });
 export default Untitled2;
