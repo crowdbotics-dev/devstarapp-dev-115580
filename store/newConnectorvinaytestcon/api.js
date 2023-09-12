b902d183ca87b99a7835bc80183aa26ba0424585
Crowdbotics <engineering@crowdbotics.com>
2023-09-12 10:06:34 +0000

    Update store.

---
+++ store/newConnectorvinaytestcon/api.js
@@ -0,0 +1,14 @@
+import axios from "axios"
+import {
+  NEW_CONNECTOR_VINAY_TESTCON_USERNAME,
+  NEW_CONNECTOR_VINAY_TESTCON_PASSWORD
+} from "react-native-dotenv"
+const newConnectorvinaytestcon = axios.create({
+  baseURL: "https://test.com",
+  auth: {
+    username: NEW_CONNECTOR_VINAY_TESTCON_USERNAME,
+    password: NEW_CONNECTOR_VINAY_TESTCON_PASSWORD
+  },
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+export const apiService = {}
