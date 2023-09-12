b86fec56e6e20db573f023a283af3fa8c2a4a7ff
Crowdbotics <engineering@crowdbotics.com>
2023-09-12 10:23:45 +0000

    Update store.

---
+++ store/newConnectortester/api.js
@@ -0,0 +1,6 @@
+import axios from "axios"
+const newConnectortester = axios.create({
+  baseURL: "https://test.co",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+export const apiService = {}
