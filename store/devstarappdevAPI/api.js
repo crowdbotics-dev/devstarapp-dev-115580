84dbb1abb69f6d1528e85731675075884b5d774e
Crowdbotics <engineering@crowdbotics.com>
2023-09-12 11:03:26 +0000

    Update store.

--- store/devstarappdevAPI/api.js
+++ store/devstarappdevAPI/api.js
@@ -3,25 +3,16 @@ const devstarappdevAPI = axios.create({
   baseURL: "https://devstarapp-dev-115580.botics.co",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
+function api_docs_schema_retrieve(payload) {
+  return devstarappdevAPI.get(`/api-docs/schema/`, {
+    params: { lang: payload.lang }
+  })
+}
 function api_v1_login_create(payload) {
   return devstarappdevAPI.post(`/api/v1/login/`, payload)
 }
 function api_v1_signup_create(payload) {
   return devstarappdevAPI.post(`/api/v1/signup/`, payload)
-}
-function rest_auth_user_retrieve(payload) {
-  return devstarappdevAPI.get(`/rest-auth/user/`)
-}
-function rest_auth_user_update(payload) {
-  return devstarappdevAPI.put(`/rest-auth/user/`, payload)
-}
-function rest_auth_user_partial_update(payload) {
-  return devstarappdevAPI.patch(`/rest-auth/user/`, payload)
-}
-function api_docs_schema_retrieve(payload) {
-  return devstarappdevAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
 }
 function rest_auth_login_create(payload) {
   return devstarappdevAPI.post(`/rest-auth/login/`, payload)
@@ -32,34 +23,43 @@ function rest_auth_logout_retrieve(payload) {
 function rest_auth_logout_create(payload) {
   return devstarappdevAPI.post(`/rest-auth/logout/`)
 }
-function rest_auth_registration_create(payload) {
-  return devstarappdevAPI.post(`/rest-auth/registration/`, payload)
+function rest_auth_password_change_create(payload) {
+  return devstarappdevAPI.post(`/rest-auth/password/change/`, payload)
 }
 function rest_auth_password_reset_create(payload) {
   return devstarappdevAPI.post(`/rest-auth/password/reset/`, payload)
 }
-function rest_auth_password_change_create(payload) {
-  return devstarappdevAPI.post(`/rest-auth/password/change/`, payload)
-}
 function rest_auth_password_reset_confirm_create(payload) {
   return devstarappdevAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+}
+function rest_auth_registration_create(payload) {
+  return devstarappdevAPI.post(`/rest-auth/registration/`, payload)
 }
 function rest_auth_registration_verify_email_create(payload) {
   return devstarappdevAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
+function rest_auth_user_retrieve(payload) {
+  return devstarappdevAPI.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(payload) {
+  return devstarappdevAPI.put(`/rest-auth/user/`, payload)
+}
+function rest_auth_user_partial_update(payload) {
+  return devstarappdevAPI.patch(`/rest-auth/user/`, payload)
+}
 export const apiService = {
+  api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
-  rest_auth_user_retrieve,
-  rest_auth_user_update,
-  rest_auth_user_partial_update,
-  api_docs_schema_retrieve,
   rest_auth_login_create,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
-  rest_auth_registration_create,
-  rest_auth_password_reset_create,
   rest_auth_password_change_create,
+  rest_auth_password_reset_create,
   rest_auth_password_reset_confirm_create,
-  rest_auth_registration_verify_email_create
+  rest_auth_registration_create,
+  rest_auth_registration_verify_email_create,
+  rest_auth_user_retrieve,
+  rest_auth_user_update,
+  rest_auth_user_partial_update
 }
