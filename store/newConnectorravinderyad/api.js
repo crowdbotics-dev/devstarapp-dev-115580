00891ff9bdbcedc6c2460a745346601c5b3855b5
Crowdbotics <engineering@crowdbotics.com>
2023-09-12 07:16:15 +0000

    Update store.

---
+++ store/newConnectorravinderyad/api.js
@@ -0,0 +1,14 @@
+import axios from "axios"
+import {
+  NEW_CONNECTOR_RAVINDER_YAD_USERNAME,
+  NEW_CONNECTOR_RAVINDER_YAD_PASSWORD
+} from "react-native-dotenv"
+const newConnectorravinderyad = axios.create({
+  baseURL: "https://testing.com",
+  auth: {
+    username: NEW_CONNECTOR_RAVINDER_YAD_USERNAME,
+    password: NEW_CONNECTOR_RAVINDER_YAD_PASSWORD
+  },
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+export const apiService = {}
